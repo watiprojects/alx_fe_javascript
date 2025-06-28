36914ea81586eeff5f5ab0677af4a9b6e904b6de
watiprojects <wati.projects01@gmail.com>
2025-06-28 15:55:52 +0200

    update script.js to use createElement and appendChild methods

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,6 +3,7 @@ Write a JavaScript file (script.js) that handles the creation and manipulation o
 Manage an array of quote objects where each quote has a text and a category. Implement functions to display a random quote and to add new quotes called showRandomQuote and createAddQuoteForm` respectively
 */
 
+
 //create array of quotes
 const quotes = [
     {text: "quote1 - author1", category: "cat1"},
@@ -21,13 +22,34 @@ const button = document.getElementById("newQuote");
 button.addEventListener("click",showRandomQuote);
     //create showRandomQuote function
 function showRandomQuote () {
+
+    // clear previous quote
+    quoteDisplayDiv.innerHTML = "";
     //generate random index which is less than the array length
     const randomIndex = Math.floor(Math.random()*quotes.length)
     //display random quote in quoteDisplayDiv
-        //generate random quote
-    randomQuote = `${quotes[randomIndex].text} <br> ${quotes[randomIndex].category}`;
-        //change content of quoteDisplayDiv
-    quoteDisplayDiv.innerHTML = randomQuote;
+        //generate random quote using current randomIndex
+        randomQuote = quotes[randomIndex].text;
+        //generate corresponding quote category
+        randomQuoteCategory = quotes[randomIndex].category
+
+        //create a p element for the quote
+        let quoteParagraph = document.createElement("p");
+        //set the text content of the p element to the randomQuote
+        quoteParagraph.textContent = quotes[randomIndex].text;
+        
+
+        //create a p element for the category
+        let categoryParagraph = document.createElement("p");
+        //set the text content of the p element to the randomQuote
+        categoryParagraph.textContent = quotes[randomIndex].category;
+
+    //Display quote and category
+        //add to quoteDisplayDiv
+        quoteDisplayDiv.appendChild(quoteParagraph);
+        //add to quoteDisplayDiv
+        quoteDisplayDiv.appendChild(categoryParagraph);
+
 }
 
 //addQuote when user adds a quote
