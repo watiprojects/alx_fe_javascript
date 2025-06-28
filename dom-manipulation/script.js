9989164c3238264289699496a55ace33bb4612d5
watiprojects <wati.projects01@gmail.com>
2025-06-28 23:21:14 +0200

    integrate local and session storage

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -2,15 +2,31 @@
 Write a JavaScript file (script.js) that handles the creation and manipulation of DOM elements based on user interactions.
 Manage an array of quote objects where each quote has a text and a category. Implement functions to display a random quote and to add new quotes called showRandomQuote and createAddQuoteForm` respectively
 */
+//get quotes from local storage, if any
+const storedQuotes = localStorage.getItem("quotes");
 
+let quotes = [];
 
-//create array of quotes
-const quotes = [
+//parse stored quotes into an object from a string
+const parsedQuotes = JSON.parse(storedQuotes);
+
+console.log[parsedQuotes];
+
+//if stored quotes is not empty then use it as the quotes array, if not set the default quotes
+
+if (storedQuotes !== null){
+    quotes = parsedQuotes;
+} else {
+    //create array of quotes
+    quotes = [
     {text: "quote1 - author1", category: "cat1"},
     {text: "quote2 - author2", category: "cat2"},
     {text: "quote3 - author3", category: "cat1"},
     {text: "quote4 - author4", category: "cat3"},
 ]
+}
+console.log(quotes);
+ 
 
 //select quoteDisplay div
     const quoteDisplayDiv = document.getElementById("quoteDisplay");
@@ -50,6 +66,15 @@ function showRandomQuote () {
         //add to quoteDisplayDiv
         quoteDisplayDiv.appendChild(categoryParagraph);
 
+
+    //create latestQuote object
+    let latestQuote = {quote: randomQuote, category: randomQuoteCategory};
+
+    //practice using session storage 
+    //save latest quote
+    sessionStorage.setItem("latestQuoteStored", JSON.stringify(latestQuote));
+    console.log(latestQuote);
+
 }
 
 //addQuote when user adds a quote
@@ -69,11 +94,20 @@ function addQuote() {
     //add newQuote text and category to array. 
     quotes.push({text: newQuoteTextInput, category: newQuoteCategoryInput});
 
+    //save quotes to local storage
+    //turn array into a string
+    const jsonString = JSON.stringify(quotes);
+    localStorage.setItem("quotes", jsonString)
+    console.log(localStorage.quotes);
+
     //clear input fields
     newQuoteText.value = "";
     newQuoteCategory.value = "";
     }
 }
+
+
+    
 
 
 
