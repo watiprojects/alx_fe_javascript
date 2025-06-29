e7db56c90a49ed106c72fd339ee016cc32fed2f5
watiprojects <wati.projects01@gmail.com>
2025-06-29 22:10:03 +0200

    testing API

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -182,8 +182,13 @@ function filterQuotes (){
     
 }
 
+function fetchQuotesFromServer (){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
 
-
+fetchQuotesFromServer();
 
 
 
